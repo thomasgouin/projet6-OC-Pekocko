d51c3eefa9ec6c33210377a876c9d2f09a4e42fa
Thomas Gouin <thomas.gouin@gmail.com>
2020-07-02 10:46:55 +0200

    création de app.js avec express bodyParser et réglage CORS

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use((req,res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.use((req,res, next)=>{
+    console.log('Requête reçue');
+    next();
+});
+
+app.use((req, res, next)=>{
+    res.json()
+});
+
+module.exports = app; 
