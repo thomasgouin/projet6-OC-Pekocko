44712d7b90e4cfd906735f923d5eff53d73569f2
Thomas Gouin <thomas.gouin@gmail.com>
2020-07-02 15:09:22 +0200

    ajout des images dans app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const saucesRoutes = require('./routes/Sauces');
 const usersRoutes = require('./routes/Users');
@@ -16,15 +17,17 @@ const app = express();
 
 app.use((req,res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
 app.use(bodyParser.json());
 
-app.use('/api/sauces', saucesRoutes), 
-app.use('/api/auth', usersRoutes),
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', usersRoutes);
 
 
 module.exports = app; 
