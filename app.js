43c9071a7674460790d1dd4f019ff2c984988698
Thomas Gouin <thomas.gouin@gmail.com>
2020-07-02 11:05:03 +0200

    ajout de la connexion à Mongo DB

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb+srv://gouinThomas:SyHwdrX3QSOXpF2l@cluster0-hqadt.mongodb.net/Pekocko?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true})
+    .then(()=>console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req,res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin X-Requested-With, Content, Accept, Content-Type, Authorization');
