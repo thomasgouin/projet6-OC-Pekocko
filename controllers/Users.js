569cdf1b8d5df8dd4a4f07717b891522fd8cc125
Thomas Gouin <thomas.gouin@gmail.com>
2020-07-02 16:11:56 +0200

    correction faute controllers/Users.js

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
-const users = require('../models/Users');
+const jwt = require('jsonwebtoken');
 const Users = require('../models/Users');
+
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -13,11 +14,30 @@ exports.signup = (req, res, next) => {
         .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
         .catch(error => res.status(400).json({error}));
     })
-    .catch(err => res.status(500).jon({error}));
+    .catch(error => res.status(500).jon({error}));
 };
 
 exports.login = (req, res, next) => {
     Users.findOne({email: req.body.email})
-    .then()
+    .then(user =>{
+        if(!user){
+            return res.status(401).json({error: 'Utilisateur non trouvé !'});
+        }
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid =>{
+            if(!valid){
+                return res.status(401).json({error: 'Mot de passe incorrect !'});
+            }
+            res.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                    {userId: user._id},
+                    'RANDOM_TOKEN_SECRET',
+                    {expiresIn: '24h'}
+                )
+            });
+        })
+        .catch(error => res.status(500).json({error}));
+    })
     .catch(error => res.status(500).json({error}));
 };
