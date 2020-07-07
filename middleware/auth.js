1a1140cae1f7e26549885a4841b697510a6c447b
Thomas Gouin <thomas.gouin@gmail.com>
2020-07-07 13:34:54 +0200

    correction faute de frappe auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        const userID = decodedToken.userId;
+        const userId = decodedToken.userId;
         if (req.body.userId && req.body.userId !== userId){
             throw 'User Id non valable !';
         } else {
