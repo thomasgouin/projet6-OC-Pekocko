ed225ea0784debc345cdf83f10aa3e16ebf34796
Thomas Gouin <thomas.gouin@gmail.com>
2020-07-07 13:37:34 +0200

    réorganisation des fichiers server et app

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ mongoose.connect('mongodb+srv://gouinThomas:SyHwdrX3QSOXpF2l@cluster0-hqadt.mong
 
 const app = express();
 
-app.use((req,res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 app.use(bodyParser.json());
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const normalizePort = val =>{
+const normalizePort = (val) =>{
     const port = parseInt(val, 10);
 
     if(isNaN(port)){
@@ -16,23 +16,23 @@ const normalizePort = val =>{
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-const errorHandler = error =>{
+const errorHandler = (error) =>{
     if (error.syscall !== 'listen'){
         throw error;
     }
     const address = server.address();
-    const bind = typeof address ==='string' ? 'pipe ' + address : 'port ' + port;
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
     switch(error.code){
-        case 'EACCESS':
-            console.error(bind + 'requires elevated privileges.');
-            process.exit(1);
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.')
+            process.exit(1)
             break;
         case 'EADDRINUSE': 
-            console.error(bind + ' requires elevated privileges.');
-            process.exit(1);
+            console.error(bind + ' is already in use.')
+            process.exit(1)
             break;
         default: 
-            throw error;
+            throw error
     }
 };
 
