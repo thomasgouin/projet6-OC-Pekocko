3d52488165baedcbad8f96293c65df5e21625a7f
Thomas Gouin <thomas.gouin@gmail.com>
2020-07-07 13:33:57 +0200

    correction faute de frappe

--- controllers/Sauces.js
+++ controllers/Sauces.js
@@ -1,14 +1,13 @@
-const Sauce = require('../models/Sauces');
+const Sauces = require('../models/Sauces');
 const fs = require('fs');
 
 exports.createSauce = (req, res, next)=>{
     const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
-    const sauce = new Sauce({
+    const sauce = new Sauces({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    thing.save()
+    sauce.save()
         .then(()=> res.status(201).json({message: 'Sauce enregistrée'}))
         .catch(error => res.status(400).json({error}));
 };
@@ -16,20 +15,24 @@ exports.createSauce = (req, res, next)=>{
 exports.modifySauce = (req, res, next) =>{
     const sauceObject = req.file ?
     {
-        ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : {...req.body};
-    Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
-    .then(()=> res.status(200).json({message : 'Sauce modifiée !'}))
-    .catch(error => res.status(400).json({error}));
-};
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+
+    Sauces.updateOne(
+        { _id: req.params.id },
+        { ...sauceObject, _id: req.params.id }
+    )
+    .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+    .catch((error) => res.status(400).json({ error }))
+}
 
 exports.deleteSauce = (req, res, next) =>{
-    Sauce.findOne({ _id: req.params.id})
+    Sauces.findOne({ _id: req.params.id})
     .then(sauce =>{
         const filename = sauce.imageUrl.split('/images/')[1];
         fs.unlink(`images/${filename}`, () =>{
-            Sauce.deleteOne({_id: req.params.id})
+            Sauces.deleteOne({_id: req.params.id})
             .then(()=> res.status(200).json({message: 'Sauce supprimée !'}))
             .catch(error => res.status(400).json({error}));
         });
@@ -38,13 +41,19 @@ exports.deleteSauce = (req, res, next) =>{
 };
 
 exports.getOneSauce = (req, res, next) =>{
-    Sauce.findOne({_id: req.params.id})
-    .then(sauce =>res.status(200).json({sauce}))
-    .catch(error => res.status(404).json(error));
+    Sauces.findOne({_id: req.params.id})
+    .then(sauce =>res.status(200).json(sauce))
+    .catch(error => res.status(404).json({error}));
 };
 
 exports.getAllSauces = (req, res, next) =>{
-    Sauce.find()
-    .then(sauces =>res.status(200).json({sauces}))
-    .catch(error => res.status(404).json(error));
+    Sauces.find()
+    .then(sauces =>res.status(200).json(sauces))
+    .catch(error => res.status(404).json({error}));
 };
+
+/*
+exports.setLikesDislikes = (req, res, next) =>{
+
+}
+*/
