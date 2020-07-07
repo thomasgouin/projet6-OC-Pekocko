950ad6778641f2df6799975475e3a6e142d756ef
Thomas Gouin <thomas.gouin@gmail.com>
2020-07-07 13:36:38 +0200

    prédéfinition de la route like dislikes dans Sauces.js

--- routes/Sauces.js
+++ routes/Sauces.js
@@ -5,12 +5,11 @@ const saucesCtrl = require('../controllers/Sauces');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-
+router.get('/', auth, saucesCtrl.getAllSauces);
 router.post('/', auth, multer, saucesCtrl.createSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
-router.get('/:id', auth, saucesCtrl.getOneSauce);
-router.get('/', auth, saucesCtrl.getAllSauces);
-router.post('/:id/like', auth, saucesCtrl.addLikesDislikes);
+//router.post('/:id/like', auth, saucesCtrl.setLikesDislikes);
 
 module.exports = router; 
